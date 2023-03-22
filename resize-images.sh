3a39499956967644acd8861d36eafc2c3e490179
prehensile <me@prehensile.co.uk>
2023-03-22 00:44:43 +0000

    image resize script

---
+++ resize-images.sh
@@ -0,0 +1,2 @@
+mogrify -resize 1280 /home/prehensile/src/static-timeline-generator/_site/img/content/*.png 
+mogrify -quality 75 -resize 1280 /home/prehensile/src/static-timeline-generator/_site/img/content/*.jpg
